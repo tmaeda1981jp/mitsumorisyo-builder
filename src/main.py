d3017e9ac258b04b33dce4ec8d5eda304d88b420
tmaeda1981jp <tmaeda1981jp@gmail.com>
2014-04-14 07:57:25 +0900

    Add to handle IOError on main

--- src/main.py
+++ src/main.py
@@ -3,19 +3,23 @@
 
 from company import Company
 from quotation import Quotation
+import argparse
 
 if __name__ == '__main__':
-
-    import argparse
-    parser = argparse.ArgumentParser(description='mitsumorisyo builder')
+    parser = argparse.ArgumentParser(description='quotation-sheet-builder')
     parser.add_argument('-c', '--client', required=True)
     parser.add_argument('-m', '--month', type=int)
     args = parser.parse_args()
     print(args)
 
-    contractor = Company('./contractor.yaml')
-    client = Company('./client/%s/client.yaml' % args.client)
-    quotation = Quotation('./client/%s/%s.yaml' % (args.client, args.month))
+    try:
+        contractor = Company('./contractor.yaml')
+        client = Company('./client/%s/client.yaml' % args.client)
+        quotation = Quotation('./client/%s/%s.yaml' %
+                              (args.client, args.month))
+    except IOError as message:
+        print message
+        exit(1)
 
     print unicode(contractor)
     print unicode(client)
